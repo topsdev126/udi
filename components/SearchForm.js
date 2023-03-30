037e0c440287aa6fb81fcc3aecbca497eef22fae
topsdev126 <khaledjamal5946@gmail.com>
2023-03-30 09:39:43 -0400

    finished

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { toast } from 'react-toastify';
 
 const SearchForm = ({ onSubmit }) => {
   const [topic, setTopic] = useState('');
@@ -7,32 +8,41 @@ const SearchForm = ({ onSubmit }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    const api_key = 'AqZRa0FXlQBCUTxt0faLA6w9bfzdLnT5_HvyPiRVGKJzNyKhn37rD8_jpppGJ6mU'
-    const response = await fetch(`https://dev.virtualearth.net/REST/v1/Locations?q=${location}&key=${api_key}`);
-    const data = await response.json();
-    const locationCoordinates = data.resourceSets[0].resources[0].point.coordinates;
-
-    onSubmit({ topic, location, distance, locationCoordinates });
+    if(!topic || !location || !distance){
+      toast.success('Pls fill all inputs !');
+    }else{
+      const api_key = 'AqZRa0FXlQBCUTxt0faLA6w9bfzdLnT5_HvyPiRVGKJzNyKhn37rD8_jpppGJ6mU'
+      const response = await fetch(`https://dev.virtualearth.net/REST/v1/Locations?q=${location}&key=${api_key}`);
+      const data = await response.json();
+      const locationCoordinates = data?.resourceSets[0]?.resources[0]?.point?.coordinates;
+  
+      onSubmit({ topic, location, distance, locationCoordinates });
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="topic">Topic</label>
-        <input type="text" id="topic" value={topic} onChange={(e) => setTopic(e.target.value)} />
+    <form onSubmit={handleSubmit} className="pb-1 space-y-6 grid grid-cols-12 gap-0 lg:grid-cols-12">
+      <div className=' w-full col-span-12 p-4 mx-auto lg:col-span-12 xl:p-12 md:w-2/6'>
+        <label className="  block">
+          <span className=" mb-1 text-sm font-medium text-gray-600">Topic :</span>
+          <input className="w-full px-3 py-3 rounded-lg bg-gray-50 placeholder:text-gray-400 outline-none  hover:border-red-500  border-2" type="text" id="topic" value={topic} onChange={(e) => setTopic(e.target.value)} />
+        </label>
+
+        <label className="  block">
+          <span className=" mb-1 text-sm font-medium text-gray-600">Location :</span>
+          <input className="w-full px-3 py-3 rounded-lg bg-gray-50 placeholder:text-gray-400 outline-none  hover:border-red-500  border-2" type="text" id="location" value={location} onChange={(e) => setLocation(e.target.value)} />
+        </label>
+
+        <label className="  block">
+          <span className=" mb-1 text-sm font-medium text-gray-600">Distance in meters :</span>
+          <input type="text" className="w-full px-3 py-3 rounded-lg bg-gray-50 placeholder:text-gray-400 outline-none  hover:border-red-500  border-2" id="distance" value={distance} onChange={(e) => setDistance(e.target.value)} />
+        </label>
+      <button className="mt-2 bg-red-600 text-white font-bold text-md rounded p-2 disabled:bg-red-400 " type="submit">Search</button>
       </div>
-      <div>
-        <label htmlFor="location">Location</label>
-        <input type="text" id="location" value={location} onChange={(e) => setLocation(e.target.value)} />
-      </div>
-      <div>
-        <label htmlFor="distance">Distance</label>
-        <input type="text" id="distance" value={distance} onChange={(e) => setDistance(e.target.value)} />
-      </div>
-      <button type="submit">Search</button>
     </form>
+
   );
 };
 
 export default SearchForm;
+
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,85 @@
-import Head from 'next/head'
+import SearchForm from '../components/SearchForm';
+import { useState } from 'react';
 
-export default function Home() {
+export default function Search() {
+  const [results, setResults] = useState([]);
+
+  const handleSearch = async (searchParams) => {
+    const { topic, location, distance } = searchParams;
+
+    const api_key = 'AqZRa0FXlQBCUTxt0faLA6w9bfzdLnT5_HvyPiRVGKJzNyKhn37rD8_jpppGJ6mU'
+    const response = await fetch(`https://dev.virtualearth.net/REST/v1/Locations?q=${topic},${location}&key=${api_key}`);
+    const data = await response.json();
+    const locations = data?.resourceSets[0]?.resources;
+
+    // Filter locations by distance
+    const filteredLocations = locations.filter((location) => {
+      const distanceToLocation = location?.point?.coordinates.reduce((prev, curr, i) => {
+        const diff = curr - searchParams?.locationCoordinates[i];
+        return prev + diff * diff;
+      }, 0) ** 0.5;
+
+      return distanceToLocation <= distance;
+    });
+
+    const formattedResults = filteredLocations.map((result) => ({
+      name: result.name,
+      address: result?.address?.formattedAddress,
+      location: result?.point?.coordinates?.reverse().join(', ')
+    }));
+
+    setResults(formattedResults);
+  };
+
   return (
-    <div>
-      <Head>
-        <title>My Next.js App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <main className=" space-y-6">
+        <SearchForm onSubmit={handleSearch} />
+        {results.length === 0 ? (
+          <div className="flex  gap-6 w-full  ">
+            <div className="mx-auto  rounded-lg ">
+              <h1 className="mx-3 text-lg font-semibold ">Not found result</h1>
 
-      <main>
-        <h1>Welcome to my app!</h1>
+            </div>
+          </div>
+        ) : (
+
+          <section className="flex gap-6 w-full  ">
+            {results.map((result, index) => (<div key={index} className=" mx-auto overflow-hidden bg-white rounded-lg shadow-lg ">
+
+
+              <div className="flex items-center px-6 py-3 bg-red-500">
+                <h1 className="mx-3 text-lg font-semibold text-white">Go to Map </h1>
+
+              </div>
+
+
+              <div className="px-6 py-4" >
+
+                <h1 className="text-xl font-semibold text-gray-800 dark:text-white"> Name: </h1>
+                <p className="py-2 text-gray-700 mt-2">{result.name}</p>
+                <br />
+
+                <h1 className="text-xl font-semibold text-gray-800 dark:text-white"> Address: </h1>
+                <p className="py-2 text-gray-700 mt-2">{result.address}</p>
+                <br />
+
+                <h1 className="text-xl font-semibold text-gray-800 dark:text-white"> Location: </h1>
+                <p className="py-2 text-gray-700 mt-2">{result.location}</p>
+                <br />
+
+
+              </div>
+
+            </div>
+            ))}
+          </section>
+
+
+
+
+        )}
       </main>
-    </div>
-  )
+    </>
+  );
 }
