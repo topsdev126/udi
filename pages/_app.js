f457b99c6012abfc62c4ffc585a2380f98068c82
topsdev126 <khaledjamal5946@gmail.com>
2023-03-31 06:41:09 -0400

    last change

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '@/styles/globals.css'
-import 'react-bingmaps/lib/components/ReactBingmaps/ReactBingmaps.css'
 import  {ToastContainer}  from 'react-toastify';
 import "react-toastify/dist/ReactToastify.min.css"
 
